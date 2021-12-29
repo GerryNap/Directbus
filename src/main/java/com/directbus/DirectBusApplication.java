db24dc2c655d56c60921606557a3eef6acde3c97
gerry.nap98@gmail.com <gerry.nap98@gmail.com>
2021-12-29 15:49:21 +0100

    No pocu e tuttu

---
+++ src/main/java/com/directbus/DirectBusApplication.java
@@ -0,0 +1,13 @@
+package com.directbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DirectBusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/directbus/DirectBusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.directbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DirectBusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

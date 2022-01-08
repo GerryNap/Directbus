0c6ac655932a66bc44ee6558508a6eec334903ac
domenicolorenti <83828595+domenicolorenti@users.noreply.github.com>
2022-01-08 16:38:53 +0100

    database

--- src/main/java/com/directbus/DirectBusApplication.java
+++ src/main/java/com/directbus/DirectBusApplication.java
@@ -3,6 +3,8 @@ package com.directbus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.directbus.database.DataBaseHandler;
+
 @SpringBootApplication
 public class DirectBusApplication {
 

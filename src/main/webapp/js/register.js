4ffa2b751088e4835b6233c476ad11200c294bed
gerry.nap98@gmail.com <gerry.nap98@gmail.com>
2022-01-11 11:51:24 +0100

    Registration (Form validation)

--- src/main/webapp/js/register.js
+++ src/main/webapp/js/register.js
@@ -2,9 +2,32 @@ $(document).ready(
     function() {
         $("#register-form").on("submit", function(event) {
             event.preventDefault();
-            console.log("registering");
-            ajaxRegisterPost();
+            if(formValidation())
+            	ajaxRegisterPost();
         });
+        
+        function formValidation(){
+			var firstName = document.getElementById("FirstName").value;
+			if(stringContainsNumber(firstName)){
+				window.alert("Inserisci nuovamente il tuo nome");
+				return false;
+			}
+			
+			var lastName = document.getElementById("LastName").value;
+			if(stringContainsNumber(lastName)){
+				window.alert("Inserisci nuovamente il tuo cognome");
+				return false;
+			}
+			
+			var psw = $("#Password").val();
+			var pswc = $("#PasswordCheck").val();
+			if(psw !== pswc){
+				window.alert("Le password non coincidono");
+				return false;
+			}
+			
+			return true;
+		}
 
         function ajaxRegisterPost() {
 			var userData = {
@@ -13,21 +36,20 @@ $(document).ready(
 				email : $("#Email").val(),
                 password : $("#Password").val()
 			}
-			
         
 			$.ajax({
 				type : "POST",
 				contentType : "application/json",
 				url : "doRegistration",
-                data : JSON.stringify(userData),
-				success : function (data) {
-					if (data == "success") {
-                        console.log("User " + userData.email + " successfully registered.");
-                        console.log("Logging in...");
-                        window.location.href = "/";
-                    }
-                }
+                data : JSON.stringify(userData)
 			});
         }
-    }
-)
+        
+        function stringContainsNumber(str){
+		  	let matchPattern = str.match(/\d+/g);
+		  	if (matchPattern != null)
+		  	  	return true;
+		   	else
+		  		return false;
+		}
+    })

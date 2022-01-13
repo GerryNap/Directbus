b82cd63722e077c7903bd18b46abd980a4cd1c56
gerry.nap98@gmail.com <gerry.nap98@gmail.com>
2022-01-13 14:10:23 +0100

    Bug fix
    Travel history

--- src/main/webapp/WEB-INF/jsp/myProfile.jsp
+++ src/main/webapp/WEB-INF/jsp/myProfile.jsp
@@ -21,12 +21,12 @@
 	    <form method="get" action="myProfile" class="position-relative">
 	    
 			<div class="container mt-5 " id="firstContainer">
-				<div class="row">											
+				<div class="row">
+				    <%@include file="profile/travelHistory.jsp"%>											
 				    <%@include file="profile/userCard.jsp"%>
 				    <%@include file="profile/accountSummary.jsp"%>	  
 				    <%@include file="profile/reservation.jsp"%>	
-				    <%@include file="profile/paymentData.jsp"%>
-				    <%@include file="profile/travelHistory.jsp"%>				  						  				 									
+				    <%@include file="profile/paymentData.jsp"%>				  						  				 									
 				</div>
 			</div>	
 		</form>	
--- src/main/webapp/js/myProfile.js
+++ src/main/webapp/js/myProfile.js
@@ -6,29 +6,29 @@ $(document).ready(
 })
 
 function loadSummary() {
-		$("#accountSummary").show();
-	 	$("#reservation").hide();
-		$("#paymentData").hide();
-		$("#travelHistory").hide();
-	}
-	
-	function loadReserveation(){
-		$("#accountSummary").hide();
-	 	$("#reservation").show();
-		$("#paymentData").hide();
-		$("#travelHistory").hide();
-	}
-	
-	function loadPaymentData(){
-		$("#accountSummary").hide();
-	 	$("#reservation").hide();
-		$("#paymentData").show();
-		$("#travelHistory").hide();
-	}
-	
-	function loadTravelHistory(){
-		$("#accountSummary").hide();
-	 	$("#reservation").hide();
-		$("#paymentData").hide();
-		$("#travelHistory").show();
-	}
+	$("#accountSummary").show();
+ 	$("#reservation").hide();
+	$("#paymentData").hide();
+	$("#travelHistory").hide();
+}
+
+function loadReserveation(){
+	$("#accountSummary").hide();
+ 	$("#reservation").show();
+	$("#paymentData").hide();
+	$("#travelHistory").hide();
+}
+
+function loadPaymentData(){
+	$("#accountSummary").hide();
+ 	$("#reservation").hide();
+	$("#paymentData").show();
+	$("#travelHistory").hide();
+}
+
+function loadTravelHistory(){
+	$("#accountSummary").hide();
+ 	$("#reservation").hide();
+	$("#paymentData").hide();
+	$("#travelHistory").show();
+}

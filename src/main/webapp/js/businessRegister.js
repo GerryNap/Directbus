b1acdc847cec798b6393b1a232c9e68a632c5e47
domenicolorenti <83828595+domenicolorenti@users.noreply.github.com>
2022-01-15 16:03:36 +0100

    reindirizzamentazione business

--- src/main/webapp/js/businessRegister.js
+++ src/main/webapp/js/businessRegister.js
@@ -37,7 +37,14 @@ $(document).ready(
 				type : "POST",
 				contentType : "application/json",
 				url : "doBusinessRegistration",
-                data : JSON.stringify(userData)
+                data : JSON.stringify(userData),
+                success: function(data, status, xhr){
+							if(data == "success")
+								window.location.href = "/";
+						 },
+				error: function(data, status, xhr){
+							window.alert("user alredy exist")
+						 }
 			});
         }
         
